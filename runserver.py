29b5484ade0e5a396560893b17ccdb264ce948ff
Gyanendra Mishra <anomaly.the@gmail.com>
2016-12-03 17:37:24 +0530

    logs to access.log instead of stdout

--- runserver.py
+++ runserver.py
@@ -8,6 +8,7 @@ import logging
 
 
 def runserver():
+    logging.basicConfig(filename='access.log', level=logging.DEBUG)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
